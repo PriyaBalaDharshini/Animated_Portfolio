5f66df7a8dfecbe80685f91ad633975b30cc6b0a
PriyaBalaDharshini <priyaarasu12@gmail.com>
2024-09-03 13:34:52 +0530

    Global css written

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./app.scss"
+
+
+function App() {
+  return (
+    <>
+      <section>Home</section>
+      <section>Skills</section>
+      <section>Project 1</section>
+      <section>Project 2</section>
+      <section>Project 3</section>
+      <section>Project 4</section>
+      <section>Education</section>
+      <section>Experience</section>
+      <section>Conatct</section>
+    </>
+  )
+}
+
+export default App
